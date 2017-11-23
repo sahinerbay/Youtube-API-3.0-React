b92a098bf4ea3d7322af5af8e6029c092b964701
sahinerbay <sahin@sahinerbay.com>
2017-11-23 21:16:30 +0200

    Install lodash to add debounce for search bar input
    Finish search bar feature

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDgmBCopkIf8l5Jjnavj2XtilYmdCp17DE';
-
-
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('surfboards'); //default search
+	}
 
-		YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-			this.setState({ videos });
+	videoSearch(term) {
+		YTSearch({ key: API_KEY, term: term }, videos => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0] //initial value
+			});
 		});
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos = {this.state.videos} />
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
 			</div>
 		);
 	}
